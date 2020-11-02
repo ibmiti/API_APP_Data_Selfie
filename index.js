545291b966f12ece8735cfbe380d4f3cc5a31ae0
oliver olive <oo@olivers-MacBook-Pro.local>
2020-11-02 16:13:20 -0500

    adding limit of 1mb for total size of incoming body to server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Represents the server
+
 const express = require('express');
 const app     = express();
 app.listen(3001, () => console.log('listening at 3001'));
 app.use(express.static('public'));
-
+app.use(express.json({ limit: '1mb'}));
 app.post('/api', (request, response) => {
+    console.log('ello I received a request');
     console.log(request);
 });
