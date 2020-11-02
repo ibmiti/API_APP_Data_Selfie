f5e73ac3bd791b3179a82065638adc5d5a3d688e
oliver olive <oo@olivers-MacBook-Pro.local>
2020-11-02 16:05:30 -0500

    sending code

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app     = express();
+app.listen(3001, () => console.log('listening at 3001'));
+app.use(express.static('public'));
+
+app.post('/api', (request, response) => {
+    console.log(request);
+});
